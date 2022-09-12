f890a4c25d70cba8c3003e73bbfc26ea8a15b2d3
Igor Perekalskiy <perekalskiy_igor@mail.ru>
2022-09-12 10:59:06 +0300

    120920221058

--- backend/interactions/apps.py
+++ backend/interactions/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RecipesConfig(AppConfig):
+class InteractionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recipes'
+    name = 'interactions'
